44e088ca9c511498843fcb7e1de04035280e2759
Quetzalcoalt <99593775+Quetzalcoalts@users.noreply.github.com>
2022-06-13 21:37:56 +0700

    Cart
    Cart blm UI

--- lib/cart.dart
+++ lib/cart.dart
@@ -3,29 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'main.dart';
 
-class Cart_Add {
-  List cart_nama = List.empty(growable: true);
-  List cart_gambar = List.empty(growable: true);
-
-  Cart_Add({required this.cart_nama, required this.cart_gambar});
-}
+List cart_nama = [];
+List cart_gambar = [];
 
 class Cart extends StatefulWidget {
-  final Cart_Add cart_troli;
-  final int item_id;
-  const Cart({Key? key, required this.cart_troli, required this.item_id})
-      : super(key: key);
+  final String id;
+  final String item_id;
+  const Cart({ Key? key, required this.id, required this.item_id}) : super(key: key);
 
   @override
   State<Cart> createState() => _CartState();
 }
 
 class _CartState extends State<Cart> {
-  void tambahdata(String a, String b) {
+  @override
+  void initState() {
+    super.initState();
+    initial();
+  }
+
+  void initial(){
     setState(() {
-      widget.cart_troli?.cart_gambar.add(a);
-      widget.cart_troli?.cart_nama.add(b);
-      widget.cart_troli?.cart_gambar.sort();
+      cart_nama.add(widget.id);
+      cart_gambar.add(widget.item_id);
     });
   }
 
@@ -36,51 +36,13 @@ class _CartState extends State<Cart> {
         elevation: 0.8,
         backgroundColor: Color.fromARGB(255, 4, 162, 241),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            //Text("${widget.cart_troli?.cart_gambar[0]}"),
-            ElevatedButton(
-              onPressed: () {
-                tambahdata("a", "b");
-              },
-              child: const Text("--- Tambah Data ---"),
-            ),
-            Expanded(
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: widget.cart_troli?.cart_gambar.length,
-                  itemBuilder: (context, index) {
-                    return Dismissible(
-                      key: Key(widget.cart_troli?.cart_gambar[index]),
-                      background: Container(
-                        padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                        alignment: Alignment.centerLeft,
-                        color: Colors.green,
-                        child: Icon(Icons.check),
-                      ),
-                      secondaryBackground: Container(
-                        padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-                        alignment: Alignment.centerRight,
-                        color: Colors.red,
-                        child: Icon(Icons.cancel),
-                      ),
-                      child: ListTile(
-                          title: Text(
-                              "${widget.cart_troli?.cart_gambar[index]} + ${widget.cart_troli?.cart_nama[index]}"),
-                          onTap: () {
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                              duration: Duration(milliseconds: 750),
-                              content: Text(
-                                  "${widget.cart_troli?.cart_gambar[index]} + ${widget.cart_troli?.cart_nama[index]}"),
-                            ));
-                          }),
-                    );
-                  }),
-            ),
-          ],
-        ),
-      ),
+      body:
+      ListView.builder(
+        itemCount: cart_nama.length,
+        itemBuilder: (context, index){
+          return Text("${cart_nama[index]}");
+        }
+      )
     );
   }
 }
