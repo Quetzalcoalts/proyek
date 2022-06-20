d230e7abc1faba256e066537d47ec16595ca11bf
Quetzalcoalt <99593775+Quetzalcoalts@users.noreply.github.com>
2022-06-20 20:46:24 +0700

    filter kureng dikit
    hai

--- lib/filter.dart
+++ lib/filter.dart
@@ -5,9 +5,16 @@ import 'package:proyekambw/main.dart';
 import 'detMakanan.dart';
 
 int check = 3;
+int counter = 0;
 List rat3 = [];
+List rat3_nama = [];
+List rat3_gam = [];
 List rat4 = [];
+List rat4_nama = [];
+List rat4_gam = [];
 List rat5 = [];
+List rat5_nama = [];
+List rat5_gam = [];
 
 class Filter extends StatefulWidget {
   final List isi;
@@ -19,18 +26,57 @@ class Filter extends StatefulWidget {
 
 class _FilterState extends State<Filter> {
   void checking3(){
+    if(rat3.length >= 1){
+      rat3.clear();
+      rat3_nama.clear();
+      rat3_gam.clear();
+    }
+    for(int i = 0; i < widget.isi.length; i++){
+      if(widget.isi[i]['content']['reviews'] != null && widget.isi[i]['content']['reviews']['averageRating'] == 3){
+        rat3.add(widget.isi[i]);
+        rat3_gam.add(widget.isi[i]['display']['images'][0]);
+        rat3_nama.add(widget.isi[i]['display']['displayName']);
+      }
+    }
+    print(rat3);
     setState(() {
       check = 3;
     });
   }
 
   void checking4(){
+    if(rat4.length >= 1){
+      rat4.clear();
+      rat4_gam.clear();
+      rat4_nama.clear();
+    }
+    for(int i = 0; i < widget.isi.length; i++){
+      if(widget.isi[i]['content']['reviews'] != null && widget.isi[i]['content']['reviews']['averageRating'] == 4 ){
+        rat4.add(widget.isi[i]);
+        rat4_gam.add(widget.isi[i]['display']['images'][0]);
+        rat4_nama.add(widget.isi[i]['display']['displayName']);
+      }
+    }
+    print(rat4);
     setState(() {
       check = 4;
     });
   }
 
   void checking5(){
+    if(rat5.length >= 1){
+      rat5.clear();
+      rat5_nama.clear();
+      rat5_gam.clear();
+    }
+    for(int i = 0; i < widget.isi.length; i++){
+      if(widget.isi[i]['content']['reviews'] != null && widget.isi[i]['content']['reviews']['averageRating'] == 5 ){
+        rat5.add(widget.isi[i]);
+        rat5_gam.add(widget.isi[i]['display']['images'][0]);
+        rat5_nama.add(widget.isi[i]['display']['displayName']);
+      }
+    }
+    print(rat5);
     setState(() {
       check = 5;
     });
@@ -61,16 +107,135 @@ class _FilterState extends State<Filter> {
           Container(
             child: check == 3
             ? Expanded(
-              child: ListView.builder(
-                itemCount: widget.isi.length,
-                itemBuilder: (BuildContext context, index){
-                  return Text("${widget.isi[index]['content']['reviews'] != null ? widget.isi[index]['content']['reviews']['averageRating'] : "2"}");
-                },
-              ),
-            )
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 20,
+                    children: List.generate(rat3.length, (index){
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(context, 
+                          MaterialPageRoute(builder: (context){
+                            return detMakanan(id: rat3, item_id: index);
+                          }));
+                        },
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Image.network("${rat3_gam[index]}",
+                                  width: 170,
+                                height: 110,
+                                  fit: BoxFit.cover,
+                                ),
+                              ],
+                            ),
+                            Center(
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                                child: Text(
+                                  "${rat3_nama[index]}",
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+                    ),
+                ),
+              )
             : check == 4 
-              ? Text("4")
-              : Text("5")
+              ? Expanded(
+                child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                  children: List.generate(rat4.length, (index){
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, 
+                        MaterialPageRoute(builder: (context){
+                          return detMakanan(id: rat4, item_id: index);
+                        }));
+                      },
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.network("${rat4_gam[index]}",
+                                width: 170,
+                                height: 110,
+                                fit: BoxFit.cover,
+                              ),
+                            ],
+                          ),
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                              child: Text(
+                                "${rat4_nama[index]}",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    );
+                  }),
+                  ),
+              )
+              )
+              : Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                  children: List.generate(rat5.length, (index){
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, 
+                        MaterialPageRoute(builder: (context){
+                          return detMakanan(id: rat5, item_id: index);
+                        }));
+                      },
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.network("${rat5_gam[index]}",
+                                width: 170,
+                                height: 110,
+                                fit: BoxFit.cover,
+                              ),
+                            ],
+                          ),
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                              child: Text(
+                                "${rat5_nama[index]}",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    );
+                  }),
+                  ),
+              )
+              )
           )
         ],
       ),
--- lib/shopping_Payment.dart
+++ lib/shopping_Payment.dart
@@ -1,94 +1,94 @@
-import 'package:flutter_stripe/flutter_stripe.dart';
-import 'package:flutter/material.dart';
-import 'package:razorpay_flutter/razorpay_flutter.dart';
+// import 'package:flutter_stripe/flutter_stripe.dart';
+// import 'package:flutter/material.dart';
+// import 'package:razorpay_flutter/razorpay_flutter.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
+// class Home extends StatefulWidget {
+//   @override
+//   _HomeState createState() => _HomeState();
+// }
 
-class _HomeState extends State<Home> {
-  TextEditingController textEditingController = new TextEditingController();
-  late Razorpay razorpay;
+// class _HomeState extends State<Home> {
+//   TextEditingController textEditingController = new TextEditingController();
+//   late Razorpay razorpay;
 
-  @override
-  void initState() {
-    super.initState();
+//   @override
+//   void initState() {
+//     super.initState();
 
-    razorpay = new Razorpay();
+//     razorpay = new Razorpay();
 
-    razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
-    razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerErrorFailure);
-    razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExternalWallet);
-  }
+//     razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
+//     razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerErrorFailure);
+//     razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExternalWallet);
+//   }
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    razorpay.clear();
-  }
+//   @override
+//   void dispose() {
+//     // TODO: implement dispose
+//     super.dispose();
+//     razorpay.clear();
+//   }
 
-  void openCheckout() {
-    var options = {
-      "key": "rzp_test_5YplbLeCMxKCEl",
-      "amount": num.parse(textEditingController.text) * 100,
-      "name": "Sample App Puchasing",
-      "description": "Payment for anything",
-      "prefill": {"contact": "2323232323", "email": "xxxxxx@gmail.com"},
-      "external": {
-        "wallets": ["paytm"]
-      }
-    };
+//   void openCheckout() {
+//     var options = {
+//       "key": "rzp_test_5YplbLeCMxKCEl",
+//       "amount": num.parse(textEditingController.text) * 100,
+//       "name": "Sample App Puchasing",
+//       "description": "Payment for anything",
+//       "prefill": {"contact": "2323232323", "email": "xxxxxx@gmail.com"},
+//       "external": {
+//         "wallets": ["paytm"]
+//       }
+//     };
 
-    try {
-      razorpay.open(options);
-    } catch (e) {
-      print(e.toString());
-    }
-  }
+//     try {
+//       razorpay.open(options);
+//     } catch (e) {
+//       print(e.toString());
+//     }
+//   }
 
-  void handlerPaymentSuccess() {
-    print("Payment success");
-  }
+//   void handlerPaymentSuccess() {
+//     print("Payment success");
+//   }
 
-  void handlerErrorFailure() {
-    print("Payment error");
-  }
+//   void handlerErrorFailure() {
+//     print("Payment error");
+//   }
 
-  void handlerExternalWallet() {
-    print("External Wallet");
-  }
+//   void handlerExternalWallet() {
+//     print("External Wallet");
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Razor Pay"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: textEditingController,
-              decoration: InputDecoration(hintText: "Amount"),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            ElevatedButton(
-              child: Text(
-                "Click Now",
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                openCheckout();
-              },
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Razor Pay"),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(30.0),
+//         child: Column(
+//           children: [
+//             TextField(
+//               controller: textEditingController,
+//               decoration: InputDecoration(hintText: "Amount"),
+//             ),
+//             SizedBox(
+//               height: 12,
+//             ),
+//             ElevatedButton(
+//               child: Text(
+//                 "Click Now",
+//                 style: TextStyle(color: Colors.white),
+//               ),
+//               onPressed: () {
+//                 openCheckout();
+//               },
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
